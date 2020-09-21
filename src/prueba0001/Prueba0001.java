0df39d6762a5efd0b8870dc1670abdbded042a57
xedinsonx <xedinsonx2@gmail.com>
2020-09-21 18:54:18 -0500

    Java

---
+++ src/prueba0001/Prueba0001.java
@@ -0,0 +1,19 @@
+
+package prueba0001;
+
+
+public class Prueba0001 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        carro car =new carro();
+        
+        car.encender();
+        car.gasolina();
+        car.apagar();
+        
+    }
+    
+}
---
+++ src/prueba0001/carro.java
@@ -0,0 +1,30 @@
+
+package prueba0001;
+
+
+public class carro {
+    
+    public carro(){
+    // constructot   
+    
+        System.out.println("Se ha construido el Objeto");
+    }
+    //metodos
+    public void avanzar(){
+        System.out.println("Estoy avanzando"); 
+    }
+    public void frenar(){
+        System.out.println("Frenar");
+    
+    }
+    public void encender(){
+        System.out.println("Encender");
+    }
+    public void apagar(){
+        System.out.println("Apagar");
+    }
+    public void gasolina(){
+        System.out.println("Como eres pobre no tengo");
+    }
+    
+}
